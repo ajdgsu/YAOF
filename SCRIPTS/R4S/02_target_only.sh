a22de54f7ded2e52a652ad9c2062d1015ccac6ab
ajdgsu <1637448903@qq.com>
2025-09-13 07:04:31 +0000

    1

--- SCRIPTS/R4S/02_target_only.sh
+++ SCRIPTS/R4S/02_target_only.sh
@@ -6,6 +6,9 @@ sed -i 's,-mcpu=generic,-march=armv8-a+crc+crypto,g' include/target.mk
 sed -i 's,kmod-r8168,kmod-r8169,g' target/linux/rockchip/image/armv8.mk
 sed -i 's/Os/O3 -march=armv8-a+crc+crypto -Wno-error -Wno-error=mismatched-new-delete/g' include/target.mk
 
+sed -i 's,define Package\/iptables\/Default,define Package\/iptables\/Default\n  CFLAGS += -O3 -funroll-loops --param max-unroll-times=8 --param max-unrolled-insns=500 --param max-average-unrolled-insns=50,g' package/network/utils/iptables/Makefile
+sed -i 's,define Package\/nftables\/Default,define Package\/nftables\/Default\n  CFLAGS += -O3 -funroll-loops --param max-unroll-times=8 --param max-unrolled-insns=500 --param max-average-unrolled-insns=50,g' package/network/utils/nftables/Makefile
+
 #Vermagic
 latest_version="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9][4-9]/p' | sed -n 1p | sed 's/v//g' | sed 's/.tar.gz//g')"
 wget https://downloads.openwrt.org/releases/${latest_version}/targets/rockchip/armv8/profiles.json
